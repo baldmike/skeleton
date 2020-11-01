95694582c8c3987b09f0b7d1fe88f555acbf9e16
Bald Mike <baldmike312@gmail.com>
2020-11-01 10:15:18 -0600

    adds not found page

---
+++ resources/js/components/NotFoundComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>404 NOT FOUND</h1>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+ 
+            }
+        },
+        methods: {
+        },
+        computed: {
+        },
+        }
+</script>
+
+<style>
+</style>
