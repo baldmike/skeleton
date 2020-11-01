449f7600dacac319caf30c1573fa42ebabf3e18b
Bald Mike <baldmike312@gmail.com>
2020-11-01 10:15:34 -0600

    adds website page

---
+++ resources/js/pages/Website.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>This is the website</h1>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+ 
+            }
+        },
+        methods: {
+        },
+        computed: {
+        },
+        }
+</script>
+
+<style>
+</style>
