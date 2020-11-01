f43ed0959ed4773c01c2593a61376c503110860f
Bald Mike <baldmike312@gmail.com>
2020-11-01 10:13:02 -0600

    adds navbar and cleans up mainapp

--- resources/js/MainApp.vue
+++ resources/js/MainApp.vue
@@ -1,19 +1,19 @@
 <template>    
-    <div class="container-fluid">        
-        <example-component v-if="!isAuthenticated" :is="LoginComponent"></example-component>
-        <router-view v-if="isAuthenticated"></router-view>
+    <div class="container-fluid">
+        <nav-bar></nav-bar>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 
-    
+    import NavBar from './components/NavBar'
     import { mapGetters, mapActions } from 'vuex';
 
     export default {
         name: "mainApp",
         components: {
-            
+            NavBar
         },
         props: ["LoginComponent"],
         computed: mapGetters(['isAuthenticated']),
