7d8c948eabb73526534d04d95be514fe5648d8fd
Bald Mike <baldmike312@gmail.com>
2020-11-01 10:14:43 -0600

    adds dashboard and login to router

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,7 +1,10 @@
 import VueRouter from 'vue-router'
+import store from './store'
 import MainApp from './MainApp.vue'
 import LoginComponent from './components/LoginComponent.vue'
 import DashboardComponent from './components/DashboardComponent.vue'
+import NotFoundComponent from './components/NotFoundComponent.vue'
+import Website from './pages/Website.vue'
 
 export const router = new VueRouter({ 
     mode: 'history',
@@ -10,11 +13,31 @@ export const router = new VueRouter({
         {
             path: '/', 
             component: MainApp,
-            props: {LoginComponent: LoginComponent},
             children: [
                 {
+                    path: '',
+                    component: Website
+                },
+                {
                     path: 'dashboard',
-                    component: DashboardComponent
+                    component: DashboardComponent,
+                    beforeEnter: (to, from, next) => {
+                        if (!store.isAuthenticated) {
+                            next({
+                                path: 'login'
+                            });
+                            return;
+                        }
+                        next();
+                    }
+                },
+                {
+                    path: 'login',
+                    component: LoginComponent
+                },
+                {
+                    path: '*',
+                    component: NotFoundComponent
                 }
             ]
         },
