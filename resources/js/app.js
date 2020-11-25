426a845caf4abb18a4bf300f3def955c2980c268
Bald Mike <baldmike312@gmail.com>
2020-11-25 15:04:21 -0600

    add notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { router } from './router'
 import store from './store'
+import Notifications from 'vue-notification'
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.use(Notifications)
+
+// this will automatically register .vue files
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Vue.component('example-component', require('./components/LoginComponent.vue').default);
 
 Vue.use(VueRouter)
 
